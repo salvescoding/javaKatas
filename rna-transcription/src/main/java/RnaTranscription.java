48c893eb76efd51c2b43b6642ab1d85014dbc1c5
salvescoding <salves2383@gmail.com>
2021-01-03 14:59:02 +0100

    Add solution for rna transcription

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,21 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 class RnaTranscription {
 
+    private final Map<String, String> RNA_TRANSCRIPTION = Map.of(
+            "G", "C",
+            "C", "G",
+            "T", "A",
+            "A", "U"
+
+    );
+
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return  Arrays.stream(dnaStrand.split(""))
+                .map(rna -> RNA_TRANSCRIPTION.getOrDefault(rna, ""))
+                .collect(Collectors.joining());
     }
 
 }
