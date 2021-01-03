83b44835906eac70bd3be34cb6b9c0e6d7274e2e
salvescoding <salves2383@gmail.com>
2021-01-03 14:38:18 +0100

    Add anagram solution kata

--- anagram/src/main/java/Anagram.java
+++ anagram/src/main/java/Anagram.java
@@ -1,10 +1,33 @@
-/*
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+import static java.util.stream.Collectors.groupingBy;
 
-Please remove this comment when submitting your solution.
+public class Anagram {
 
-*/
+
+    private final Map<String, Long> anagram;
+    private final String originalAnagram;
+
+    public Anagram(String anagram) {
+        this.anagram = Arrays.stream(anagram.toLowerCase(Locale.ROOT).split(""))
+                .collect(groupingBy(c -> c, Collectors.counting()));
+        this.originalAnagram = anagram;
+    }
+
+
+    public List<String> match(List<String> words) {
+        return words.stream()
+                .filter(word -> !word.toLowerCase(Locale.ROOT).equals(originalAnagram.toLowerCase(Locale.ROOT)))
+                .filter(this::isAnagram)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isAnagram(String word ) {
+        return Arrays.stream(word.toLowerCase(Locale.ROOT).split(""))
+                .collect(groupingBy(c -> c, Collectors.counting())).equals(anagram);
+    }
+}
